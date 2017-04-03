259e591e7b6883f62659ef746f51bebd3b010b68
kjwon15 <kjwonmail@gmail.com>
2017-04-03 22:30:45 +0900

    Add build script
    Use herokuish to build runnable docker image

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+container_id=$(docker run -d -v /tmp/cache:/tmp/cache -v "$PWD":/tmp/app gliderlabs/herokuish /build)
+docker attach "$container_id"
+docker wait "$container_id"
+
+docker commit "$container_id" postg
